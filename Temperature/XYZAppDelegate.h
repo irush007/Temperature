7b06069a8a827dde63353102a606cf4c33241a25
Eric Wu <ewu@zynga.com>
2014-01-16 10:46:10 -0800

    Initial Commit

---
+++ Temperature/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  Temperature
+//
+//  Created by Hua Wu on 1/16/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Temperature/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Temperature
+//
+//  Created by Hua Wu on 1/16/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
